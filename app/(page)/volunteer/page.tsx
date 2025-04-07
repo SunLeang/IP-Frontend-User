360c3e77f81b56174e028d4b01e48989652ace7b
SunLeang123 <leangkiyan2020@gmail.com>
2025-04-08 00:06:54 +0700

    set tailwind to version 3

--- app/(page)/volunteer/page.tsx
+++ app/(page)/volunteer/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-function page() {
+function Page() {
   return (
-    <div>Volunteer Page</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-primary">Volunteer Page</h1>
+      <div className="mt-4">
+        <p className="text-lg font-bold text-gray-700">
+          Join us in making a difference!
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default Page;
